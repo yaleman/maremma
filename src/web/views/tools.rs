26fdcbc42d37cd260c0ffe10131faf84e09bdc5b
James Hodgkinson <james@terminaloutcomes.com>
2026-08-28 17:07:13 +1000

    Allow large tools response error

--- src/web/views/tools.rs
+++ src/web/views/tools.rs
@@ -127,6 +127,7 @@ pub(crate) async fn tools_reload_config(state: &WebState) -> Result<(), MaremmaE
 }
 
 /// Seen at `/tools`
+#[allow(clippy::result_large_err)]
 pub(crate) async fn tools(
     State(state): State<WebState>,
     claims: Option<OidcClaims<EmptyAdditionalClaims>>,
